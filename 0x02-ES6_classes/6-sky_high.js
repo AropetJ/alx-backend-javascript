d23a57965820274a21d3b11dea588dd5cd799bc4
AropetJ <aropetjoel32@gmail.com>
2024-01-04 16:25:17 +0100

    Attempted task 6 - Inheritence

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,40 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors === 'number') {
+      this._floors = floors;
+    } else {
+      throw new TypeError('Floors must be a number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft === 'number') {
+      this._sqft = newSqft;
+    } else {
+      throw new TypeError('Sqft must be a number');
+    }
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors === 'number') {
+      this._floors = newFloors;
+    } else {
+      throw new TypeError('Floors must be a number');
+    }
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
