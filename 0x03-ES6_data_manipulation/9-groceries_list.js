051a0fd99fa71c44aee892e3e2b2ad6cc2afdea2
AropetJ <aropetjoel32@gmail.com>
2024-01-09 19:02:14 +0100

    Attempted task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * @returns a map of groceries with the following items(name, quantity)
+ */
+
+export default function groceriesList() {
+  const groceries = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(groceries);
+}
