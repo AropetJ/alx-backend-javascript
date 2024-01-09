1c7688e13578db2637980b229bfdd06b0ffe8806
AropetJ <aropetjoel32@gmail.com>
2024-01-09 15:42:11 +0100

    Attempted task 3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,12 @@
+/**
+ * @returns the sum of all student ids
+ */
+
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (sum, student) => sum + student.id, 0,
+    );
+  }
+  return 0;
+}
