38e2d4ddca5944a0db1c5f60d1e34285d054782e
AropetJ <aropetjoel32@gmail.com>
2024-03-01 00:55:31 +0100

    Attempted task 3

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,20 @@
+class Utils {
+  static getClassNumber(type, a, b) {
+    const an = Math.round(a);
+    const bn = Math.round(b);
+    let result = 0;
+    if (type === 'SUM') {
+      result = an + bn;
+    } else if (type === 'SUBTRACT') {
+      result = an - bn;
+    } else if (type === 'DIVIDE') {
+      if (bn === 0) {
+        return 'Error';
+      }
+      result = an / bn;
+    }
+    return result;
+  }
+}
+
+module.exports = Utils;
