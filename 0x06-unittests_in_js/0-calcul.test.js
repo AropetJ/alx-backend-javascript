de34a675ec6b28b45d138368ea36ec6e881fa6cf
AropetJ <aropetjoel32@gmail.com>
2024-03-01 00:42:05 +0100

    Attempted task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
+describe('calculateNumber rounded sum', () => {
   it('floating point whole numbers', () => {
     assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
